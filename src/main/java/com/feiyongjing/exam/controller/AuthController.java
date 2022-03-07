fb68398791a7e241e5609c7e4482ce4ff47b1697
feiyongjing <2059326740@qq.com>
2022-03-07 21:11:08 +0800

    修改1

--- src/main/java/com/feiyongjing/exam/controller/AuthController.java
+++ src/main/java/com/feiyongjing/exam/controller/AuthController.java
@@ -147,6 +147,11 @@ public class AuthController {
         SecurityUtils.getSubject().logout();
     }
 
+
+
+
+
+
 //    @GetMapping("/user")
 //    public Response<LoginResponse> getLoginUser() {
 //        User user = UserContext.getCurrentUser();
